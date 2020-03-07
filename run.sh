7b2ec2d38d0a74cb28bb9a53274a843d4e86d588
hugotrassoudaine <hugo.trassoudaine@etud.univ-jfc.fr>
2020-03-07 11:12:11 +0100

    Add app skeleton

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-node .
+NODE_ENV='development' nodemon .
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
+// Libs
 const express = require('express');
 const path = require('path');
-const app = express();
 
+
+const app = express();
 const port = 8080;
 
+
+// Application setup
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../views'));
-
+app.locals.basedir = app.get('views');
 
 app.listen(port, function () {
     console.log(`Listening on port ${port}`);
 });
 
+
+// Pages
 app.get('/', function (req, res) {
-    res.render('test');
+    res.render('root/index');
 });
 
 app.get('/greta-moche', function (req, res) {
